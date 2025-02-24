cb6bd9e0f8d14ef940b6e14a5d02dbe2c72103e4
Iliyan Malchev <malchev@gmail.com>
2025-02-23 16:39:30 -0800

    add a simple layout test
    Signed-off-by: Iliyan Malchev <malchev@gmail.com>

---
+++ test/clojure/layout_test.clj
@@ -0,0 +1,17 @@
+;; Simple layout test
+(let [activity *context*
+      content *content-layout*]
+  
+  ;; Create a simple TextView
+  (let [text-view (doto (android.widget.TextView. activity)
+                    (.setText "Hello Weather!")
+                    (.setTextSize 32.0)
+                    (.setLayoutParams (android.widget.LinearLayout$LayoutParams.
+                                     android.widget.LinearLayout$LayoutParams/MATCH_PARENT
+                                     android.widget.LinearLayout$LayoutParams/WRAP_CONTENT)))]
+    
+    ;; Clear and add the view
+    (.removeAllViews content)
+    (.addView content text-view))
+  
+  "Test layout added") 
