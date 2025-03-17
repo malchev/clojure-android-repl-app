8f8962703b75951f178703c284488cce46d3f342
Iliyan Malchev <malchev@gmail.com>
2025-03-16 23:11:44 -0700

    examples: fix feature_test.clj
    Signed-off-by: Iliyan Malchev <malchev@gmail.com>

--- examples/feature_test.clj
+++ examples/feature_test.clj
@@ -1,9 +1,9 @@
 ;; Feature test
 ;; Part 1: Test spec functionality
 (do
-  (android.util.Log/i "ClojureREPL" "Starting spec test...")
+  (android.util.Log/i "ClojureFeatureTest" "Starting spec test...")
   (require '[clojure.spec.alpha :as s])
-  (android.util.Log/i "ClojureREPL" "Spec required successfully"))
+  (android.util.Log/i "ClojureFeatureTest" "Spec required successfully"))
 
 ;; Define specs
 (do
@@ -11,23 +11,23 @@
   (s/def ::text string?)
   (s/def ::size number?)
   (s/def ::color string?)
-  (android.util.Log/i "ClojureREPL" "Specs defined successfully"))
+  (android.util.Log/i "ClojureFeatureTest" "Specs defined successfully"))
 
 ;; Test valid config
 (do
   (def valid-config {:text "Test Button" :size 24.0 :color "#4CAF50"})
   (let [valid? (s/valid? ::button-config valid-config)]
-    (android.util.Log/i "ClojureREPL" (str "Config validation result: " valid?))))
+    (android.util.Log/i "ClojureFeatureTest" (str "Config validation result: " valid?))))
 
 ;; Part 2: Test dynamic class generation
 (do
-  (android.util.Log/i "ClojureREPL" "Starting dynamic class test...")
+  (android.util.Log/i "ClojureFeatureTest" "Starting dynamic class test...")
   (def dynamic-button
-    (proxy [android.widget.Button] [*activity*]
+    (proxy [android.widget.Button] [*context*]
       (onTouchEvent [event]
-        (android.util.Log/i "ClojureREPL" "Dynamic button touched!")
+        (android.util.Log/i "ClojureFeatureTest" "Dynamic button touched!")
         (proxy-super onTouchEvent event))))
-  (android.util.Log/i "ClojureREPL" "Dynamic button class created"))
+  (android.util.Log/i "ClojureFeatureTest" "Dynamic button class created"))
 
 ;; Configure button
 (do
@@ -37,7 +37,7 @@
   (.setTextSize dynamic-button (:size valid-config))
   (.setBackgroundColor dynamic-button (android.graphics.Color/parseColor (:color valid-config)))
   (.setTextColor dynamic-button (android.graphics.Color/parseColor "#FFFFFF"))
-  (android.util.Log/i "ClojureREPL" "Button configured"))
+  (android.util.Log/i "ClojureFeatureTest" "Button configured"))
 
 ;; Add to layout
 (do
@@ -45,9 +45,9 @@
                android.widget.LinearLayout$LayoutParams/MATCH_PARENT
                android.widget.LinearLayout$LayoutParams/WRAP_CONTENT))
   (.setMargins params 20 20 20 20)
-  (.post *root-layout*
+  (.post *content-layout*
     (proxy [java.lang.Runnable] []
       (run []
-        (.addView *root-layout* dynamic-button params)
-        (android.util.Log/i "ClojureREPL" "Dynamic button added to layout"))))
+        (.addView *content-layout* dynamic-button params)
+        (android.util.Log/i "ClojureFeatureTest" "Dynamic button added to layout"))))
   "Test completed - check logcat for results")
